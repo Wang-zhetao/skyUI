30066de5de9a9788d8208b9d0a2374f777081502
king-open <kingwang.0624@gmail.com>
2022-09-23 06:52:42 +0800

    初始化 vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>导航栏 | 
+  <router-link to="/">one</router-link> | 
+  <router-link to="/xxx">two</router-link></div>
+  <hr/>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
